7956f1e5c258e70574ba0767013035a26602f6de
starglow-it <starglowventures@gmail.com>
2024-08-16 23:31:45 -0500

    edited example.env, install.bat and run.bat

--- install.bat
+++ install.bat
@@ -1,9 +1,9 @@
 @echo off
 echo Installing virtual environment...
-python -m venv venv
+python -m venv .venv
 
 echo Activating virtual environment...
-call venv\Scripts\activate.bat
+call .venv\Scripts\activate.bat
 
 echo Installing required packages...
 pip install -r requirements.txt
--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Activating virtual environment...
-call venv\Scripts\activate.bat
+call .venv\Scripts\activate.bat
 
 echo Running python script...
 python src\main.py
